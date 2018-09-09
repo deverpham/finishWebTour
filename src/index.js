218de293144b997f510eaf4bb9195a3c54302c6c
deverpham <deverpham@gmail.com>
2018-09-09 23:26:07 +0700

    upda

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-
-function App() {
-  return <div>Hello worlds</div>;
+class App extends React.Component {
+  render() {
+    return <div>App</div>;
+  }
 }
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
